1aa36cc43ee105ba32e22e5f030140722b00d267
Sivarepo24 <sivaravichandran11@gmail.com>
2025-05-09 11:17:57 +0530

    changes in jenkins

--- build/build.sh
+++ build/build.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 
-# Get current Git branch
-BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
-
-
-echo "Building Docker image for branch: $BRANCH_NAME"
-
-# Set Docker image name based on branch
-if [ "$BRANCH_NAME" == "main" ]; then
+if [[ "$GIT_BRANCH" == "origin/main" ]]; then
   IMAGE_NAME="sivakumar135/guvi_project_prod:latest"
-  
-else
+  docker build -t react_app .
+  docker tag react_app "$IMAGE_NAME"
+  echo "Image tagged as $IMAGE_NAME"
+
+elif [[ "$GIT_BRANCH" == "origin/stage_dev" ]]; then
   IMAGE_NAME="sivakumar135/guvi_project_dev:latest"
+  docker build -t react_app .
+  docker tag react_app "$IMAGE_NAME"
+  echo "Image tagged as $IMAGE_NAME"
 fi
-
-# Build the image
-docker build -t react_app .
-docker tag react_app $IMAGE_NAME
-
-echo "Image tagged as $IMAGE_NAME"
